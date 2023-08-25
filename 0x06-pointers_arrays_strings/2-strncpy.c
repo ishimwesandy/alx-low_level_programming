d67f9287b759b6eadd8a0e9c50e6139a56f91a5b
root <Sandrine Ishimwe>
2023-08-25 09:35:37 +0000

    2. strncpy -function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,31 +1,27 @@
 #include "main.h"
 /**
- * _strncat - function that concatenate two strings
- * using at most num bytes from start
+ * _strncpy - copy a string
  * @end: input value
  * @start: input value
  * @num: input value
  *
  * Return: end
  */
-char *_strncat(char *end, char *start, int num)
+char *_strncpy(char *end, char *start, int num)
 {
-	int i;
 	int j;
 
-	i = 0;
-	while (end[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
 	while (j < num && start[j] != '\0')
 	{
-	end[i] = start[j];
-	i++;
-	j++;
+		end[j] = start[j];
+		j++;
 	}
-	end[i] = '\0';
+	while (j < num)
+	{
+		end[j] = '\0';
+		j++;
+	}
+
 	return (end);
 }
-
