580abdaa6c26e5e78329bc4a295d0395d680943b
root <Sandrine Ishimwe>
2023-09-11 11:32:42 +0000

    0-hash_table_create task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that  creates  hashed table
+ * @size: size of the array table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tb;
+
+	if (size == 0)
+		return (NULL);
+
+	tb = calloc(1, sizeof(hash_table_t));
+	if (tb == NULL)
+		return (NULL);
+	tb->size = size;
+	tb->array = calloc(size, sizeof(hash_node_t *));
+	if (tb->array == NULL)
+	{
+		free(tb);
+		return (NULL);
+	}
+	return (tb);
+}
